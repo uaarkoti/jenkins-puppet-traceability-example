bd7364f979b0288397373fd1223b435e1863920b
harpreetsingh <harpreet@cloudbees.com>
2014-05-18 02:14:48 +0000

    installed war on tomcat

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -1,7 +1,11 @@
 # manage filetest
 class tomcat7 {
+
+      package { 'wget':
+      	      ensure => installed,
+      }
       package { 'openjdk-7-jdk':
-      	      ensure => present, 
+      	      ensure => installed, 
       }
 
       package { 'tomcat7':
@@ -11,5 +15,24 @@ class tomcat7 {
       service { 'tomcat7':
       	      ensure => running, 
 	      require => Package['tomcat7'],
+      }
+
+      exec { 'sample-war':
+      	      require => [ 
+	      	      Service['tomcat7'], 
+		      Package['wget']
+	      ],
+	      command => 'wget http://10.0.2.2:8080/job/PuppetJavaSample/lastSuccessfulBuild/artifact/target/traceability.war -P /tmp/',
+	      path => ['/usr/bin'],
+      }
+
+      exec { 'delete-war-from-webapps':
+	      command => 'rm -rf /var/lib/tomcat7/webapps/traceability*',
+	      path => ['/bin'],
+      }
+
+      file { '/var/lib/tomcat7/webapps/traceability.war':
+      	   source => '/tmp/traceability.war',
+	   require => Exec['sample-war', 'delete-war-from-webapps'],
       }
 }
